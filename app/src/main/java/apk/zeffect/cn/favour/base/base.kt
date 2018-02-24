c00fe75835f59f6936ae915182d44e98f161c872
Zeffect <625417632@qq.com>
2018-02-24 20:17:51 +0800

    Initial commit

---
+++ app/src/main/java/apk/zeffect/cn/favour/base/base.kt
@@ -0,0 +1,8 @@
+package apk.zeffect.cn.favour.base
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity
+
+/**
+ * Created by Zeffect on 2018/2/24.
+ */
+class BaseActivity :SwipeBackActivity()
